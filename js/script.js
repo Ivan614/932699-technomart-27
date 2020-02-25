6ca57a8ba1cc073bcfcc7834dbfe4408b00f54e3
IvanPs <rexon574@gmail.com>
2020-02-25 19:49:43 +0300

    Небольшие правки, добавил сжатый js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+// script for order
+var btnGoodsBuy = document.querySelectorAll(".button-goods-buy");
+var popupOrder = document.querySelector(".popup-order");
+var closeOrder = popupOrder.querySelector(".popup-order-close");
+var btnOrderContinue = popupOrder.querySelector(".button-popup-order-continue");
+
+btnGoodsBuy.forEach(function (entry) {
+    entry.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupOrder.classList.add("modal-show");
+   });
+});
+
+closeOrder.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupOrder.classList.remove("modal-show");
+});
+
+btnOrderContinue.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupOrder.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (popupOrder.classList.contains("modal-show")) {
+			popupOrder.classList.remove("modal-show")
+		}
+	}
+});
+
+//script for form message
 var buttonMessage = document.querySelector(".button-message");
 var formPopup = document.querySelector(".form-message");
 var closeForm = formPopup.querySelector(".form-message-close");
@@ -15,7 +48,7 @@ try {
   } 
     catch (err) {
     isStorageSupport = false;
-}
+};
 
 buttonMessage.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -53,6 +86,30 @@ window.addEventListener("keydown", function (evt) {
 		if (formPopup.classList.contains("modal-show")) {
 			formPopup.classList.remove("modal-show")
 			formPopup.classList.remove("modal-error");
+		}
+	}
+});
+
+// script for map
+var buttonMap = document.querySelector(".button-map");
+var popupMap = document.querySelector(".popup-map");
+var closeMap = popupMap.querySelector(".map-close");
+
+buttonMap.addEventListener("click", function (evt) {
+	 evt.preventDefault();
+	 popupMap.classList.add("modal-show");
+});
+
+closeMap.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupMap.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (popupMap.classList.contains("modal-show")) {
+			popupMap.classList.remove("modal-show")
 		}
 	}
 });
